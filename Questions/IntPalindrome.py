1139c0199d051328209eddbab6de66149744594d
yojoecool <bjp0008@auburn.edu>
2019-04-01 04:39:30 -0400

    reverse integer

--- Questions/IntPalindrome.py
+++ Questions/IntPalindrome.py
@@ -14,11 +14,36 @@ class IntPalindrome:
         compare = 0
         temp = x
         tens = math.floor(math.log10(x))
-        count = 0
 
-        while count <= tens:
+        while tens >= 0:
             compare += (10**tens) * (temp % 10)
             tens -= 1
             temp /= 10
 
         return int(compare) == x
+
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x == 0:
+            return 0
+
+        negative = 1
+        if x < 0:
+            negative = -1
+        x = abs(x)
+        compare = 0
+        tens = math.floor(math.log10(x))
+
+        while tens >= 0:
+            compare += (10**tens) * (x % 10)
+            tens -= 1
+            x /= 10
+
+        if compare >= 2**31:
+            compare = 0
+
+        return int(compare * negative)
+        
