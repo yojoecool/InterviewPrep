ebf76385374d04957922ce3a93027ba64e97c1a3
yojoecool <bjp0008@auburn.edu>
2019-04-01 13:55:37 -0400

    binary gap

--- Questions/BitWise.py
+++ Questions/BitWise.py
@@ -38,3 +38,44 @@ class BitWise:
             n //= 2
 
         return answer
+    
+    """
+    Given a positive integer N, find and return the longest distance between two consecutive 1's
+    in the binary representation of N.
+
+    If there aren't two consecutive 1's, return 0.
+    
+    example 1:
+        Input: 22
+        Output: 2
+        Explanation: 
+        22 in binary is 0b10110.
+        In the binary representation of 22, there are three ones, and two consecutive pairs of 1's.
+        The first consecutive pair of 1's have distance 2.
+        The second consecutive pair of 1's have distance 1.
+        The answer is the largest of these two distances, which is 2.
+    example 2:
+        Input: 8
+        Output: 0
+        Explanation: 
+        8 in binary is 0b1000.
+        There aren't any consecutive pairs of 1's in the binary representation of 8, so we return 0.
+    """
+    def binaryGap(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        maxLength = 0
+        start = -1
+        index = 0
+        
+        while N != 0:
+            if N % 2:
+                if start >= 0 and index - start > maxLength:
+                    maxLength = index - start
+                start = index
+            N //= 2
+            index += 1
+            
+        return maxLength
