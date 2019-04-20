6a6f8762fdb0bcd6ff0bf4add62998328574c54c
yojoecool <bjp0008@auburn.edu>
2019-04-20 04:02:54 +0000

    subsequence

---
+++ Questions/SubSequence.py
@@ -0,0 +1,35 @@
+class SubSequence:
+    """
+    Given a string s and a string t, check if s is subsequence of t.
+
+    You may assume that there is only lower case English letters in both s and t.
+    t is potentially a very long (length ~= 500,000) string, and s is a short string (<=100).
+
+    A subsequence of a string is a new string which is formed from the original string by deleting
+    some (can be none) of the characters without disturbing the relative positions of the
+    remaining characters. (ie, "ace" is a subsequence of "abcde" while "aec" is not).
+
+    Example 1:
+    s = "abc", t = "ahbgdc"
+
+    Return true.
+
+    Example 2:
+    s = "axc", t = "ahbgdc"
+
+    Return false.
+    """
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        index = 0
+
+        while s and (index < len(t)):
+            if t[index] == s[0]:
+                s = s[1:]
+            index += 1
+
+        return not s
