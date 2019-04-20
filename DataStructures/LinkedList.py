80eddb7ff6898cdd9741d8f142a82e47a8fd4bb0
yojoecool <bjp0008@auburn.edu>
2019-04-20 04:22:26 +0000

    num components

--- DataStructures/LinkedList.py
+++ DataStructures/LinkedList.py
@@ -207,3 +207,54 @@ class LinkedList:
             fast = fast.next
             
         return slow
+
+    """
+    We are given head, the head node of a linked list containing unique integer values.
+
+    We are also given the list G, a subset of the values in the linked list.
+
+    Return the number of connected components in G,
+    where two values are connected if they appear consecutively in the linked list.
+
+    Example 1:
+
+    Input: 
+    head: 0->1->2->3
+    G = [0, 1, 3]
+    Output: 2
+    Explanation: 
+    0 and 1 are connected, so [0, 1] and [3] are the two connected components.
+    Example 2:
+
+    Input: 
+    head: 0->1->2->3->4
+    G = [0, 3, 1, 4]
+    Output: 2
+    Explanation: 
+    0 and 1 are connected, 3 and 4 are connected, so [0, 1] and [3, 4] are the two connected components.
+    """
+    def numComponents(self, G):
+        """
+        :type head: ListNode
+        :type G: List[int]
+        :rtype: int
+        """
+        x = set(G)
+        currBuild = 0
+
+        components = 0
+        head = self.head
+
+        while head:
+            if head.val in x:
+                x.remove(head.val)
+                currBuild += 1
+            else:
+                if currBuild > 0:
+                    components += 1
+                currBuild = 0
+            head = head.next
+
+        if currBuild:
+            components += 1
+        return components
